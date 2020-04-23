3942c190423aa1a48df2a3e5c181ae280a03f001
trungpv1601 <trungpv1601@gmail.com>
2020-04-23 15:17:50 +0700

    make for telegram

--- README.md
+++ README.md
@@ -1,40 +1,36 @@
-# AWS Billing to Slack
+# AWS Billing to Telegram
 
-![image](https://user-images.githubusercontent.com/261584/66362145-3903a200-e947-11e9-91bd-6e40e5919ac4.png)
+![](screenshot.png)
 
-Sends daily breakdowns of AWS costs to a Slack channel.
+Sends daily breakdowns of AWS costs to Telegram account.
 
 # Install
 
 1. Install [`serverless`](https://serverless.com/), which I use to configure the AWS Lambda function that runs daily.
 
-    ```
-    npm install -g serverless
-    npm install
-    ```
+   ```
+   npm install -g serverless
+   npm install
+   ```
 
-1. Create an [incoming webhook](https://www.slack.com/apps/new/A0F7XDUAZ) that will post to the channel of your choice on your Slack workspace. Grab the URL for use in the next step.
+2. Create a Bot throught Bot Father and get token.
 
-1. Deploy the system into your AWS account, replacing the webhook URL below with the one you generated above.
+3. Deploy the system into your AWS account.
 
-    ```
-    serverless deploy --slack_url="https://hooks.slack.com/services/xxx/yyy/zzzz"
-    ```
+   ```
+   serverless deploy --access_key="_SECRET_" --secret_key="_SECRET_" --telegram_user="_SECRET_" --telegram_bot_token="_SECRET_"
+   ```
 
-    You can also run it once to verify that it works:
+   You can also run it once to verify that it works:
 
-    ```
-    serverless invoke --function report_cost --slack_url="https://hooks.slack.com/services/xxx/yyy/zzzz"
-    ```
+   ```
+   serverless invoke --function report_cost
+   ```
 
 ## Support for AWS Credits
 
 If you have AWS credits on your account and want to see them taken into account on this report, head to [your billing dashboard](https://console.aws.amazon.com/billing/home?#/credits) and note down the "Expiration Date", "Amount Remaining", and the "as of" date towards the bottom of the page. Add all three of these items to the command line when executing the `deploy` or `invoke`:
 
     ```
-    serverless deploy \
-        --slack_url="https://hooks.slack.com/services/xxx/yyy/zzzz" \
-        --credits_expire_date="mm/dd/yyyy" \
-        --credits_remaining_date="mm/dd/yyyy" \
-        --credits_remaining="xxx.xx"
+    serverless deploy --access_key="_SECRET_" --secret_key="_SECRET_" --telegram_user="_SECRET_" --telegram_bot_token="_SECRET_" --credits_expire_date="mm/dd/yyyy" --credits_remaining_date="mm/dd/yyyy" --credits_remaining="xxx.xx"
     ```
--- handler.py
+++ handler.py
@@ -4,29 +4,38 @@ import datetime
 import os
 import requests
 import sys
+import requests
 
 n_days = 7
 today = datetime.datetime.today()
 week_ago = today - datetime.timedelta(days=n_days)
 
-sparks = ['▁', '▂', '▃', '▄', '▅', '▆', '▇'] # Leaving out the full block because Slack doesn't like it: '█'
+sparks = ['▁', '▂', '▃', '▄', '▅', '▆', '▇']
+
 
 def sparkline(datapoints):
-    lower = min(datapoints)
-    upper = max(datapoints)
-    width = upper - lower
-    n_sparks = len(sparks) - 1
+    lowest = min(datapoints)
+    highest = max(datapoints)
+    if highest == 0:
+        highest = 1
 
     line = ""
-    for dp in datapoints:
-        scaled = 1 if width == 0 else (dp - lower) / width
-        which_spark = int(scaled * n_sparks)
-        line += (sparks[which_spark])
+    for i in range(n_days):
+        if i < len(datapoints):
+            dp = datapoints[i]
+            which_spark = int((dp / highest) * (len(sparks) - 1))
+            line += (sparks[which_spark])
+        else:
+            line += (sparks[0])
 
     return line
 
+
 def report_cost(event, context):
-    client = boto3.client('ce')
+    aws_access_key = os.environ.get('ACCESS_KEY')
+    aws_secret_key = os.environ.get('SECRET_KEY')
+    client = boto3.client('ce', aws_access_key_id=aws_access_key,
+                          aws_secret_access_key=aws_secret_key)
 
     query = {
         "TimePeriod": {
@@ -58,30 +67,36 @@ def report_cost(event, context):
 
     result = client.get_cost_and_usage(**query)
 
-    buffer = "%-40s %-7s $%5s\n" % ("Service", "Last 7d", "Yday")
+    buffer = "%-40s\t %-7s\t $%5s\n" % ("Service", "Last 7d", "Yday")
 
-    cost_per_day_by_service = defaultdict(list)
-
+    cost_by_days = []
+    services = []
     # Build a map of service -> array of daily costs for the time frame
     for day in result['ResultsByTime']:
+        cost_by_services = defaultdict(list)
         for group in day['Groups']:
             key = group['Keys'][0]
             cost = float(group['Metrics']['UnblendedCost']['Amount'])
+            cost_by_services[key] = cost
+            services.append(key)
+        cost_by_days.append(cost_by_services)
+    services = set(services)
 
-            cost_per_day_by_service[key].append(cost)
+    cost_per_day_by_service = defaultdict(list)
+    for cost_by_day in cost_by_days:
+        for service in services:
+            if (cost_by_day[service] == []):
+                cost_per_day_by_service[service].append(0.0)
+            else:
+                cost_per_day_by_service[service].append(cost_by_day[service])
 
     # Sort the map by yesterday's cost
-    most_expensive_yesterday = sorted(cost_per_day_by_service.items(), key=lambda i: i[1][-1], reverse=True)
+    most_expensive_yesterday = sorted(
+        cost_per_day_by_service.items(), key=lambda i: i[1][-1], reverse=True)
 
-    for service_name, costs in most_expensive_yesterday[:5]:
-        buffer += "%-40s %s $%5.2f\n" % (service_name, sparkline(costs), costs[-1])
-
-    other_costs = [0.0] * n_days
-    for service_name, costs in most_expensive_yesterday[5:]:
-        for i, cost in enumerate(costs):
-            other_costs[i] += cost
-
-    buffer += "%-40s %s $%5.2f\n" % ("Other", sparkline(other_costs), other_costs[-1])
+    for service_name, costs in most_expensive_yesterday:
+        buffer += "%-40s\t %s\t $%5.2f\n" % (service_name,
+                                             sparkline(costs), costs[-1])
 
     total_costs = [0.0] * n_days
     for day_number in range(n_days):
@@ -91,21 +106,26 @@ def report_cost(event, context):
             except IndexError:
                 total_costs[day_number] += 0.0
 
-    buffer += "%-40s %s $%5.2f\n" % ("Total", sparkline(total_costs), total_costs[-1])
+    buffer += "%-40s\t %s\t $%5.2f\n" % ("Total",
+                                         sparkline(total_costs), total_costs[-1])
 
     credits_expire_date = os.environ.get('CREDITS_EXPIRE_DATE')
     if credits_expire_date:
-        credits_expire_date = datetime.datetime.strptime(credits_expire_date, "%m/%d/%Y")
+        credits_expire_date = datetime.datetime.strptime(
+            credits_expire_date, "%m/%d/%Y")
 
         credits_remaining_as_of = os.environ.get('CREDITS_REMAINING_AS_OF')
-        credits_remaining_as_of = datetime.datetime.strptime(credits_remaining_as_of, "%m/%d/%Y")
+        credits_remaining_as_of = datetime.datetime.strptime(
+            credits_remaining_as_of, "%m/%d/%Y")
 
         credits_remaining = float(os.environ.get('CREDITS_REMAINING'))
 
-        days_left_on_credits = (credits_expire_date - credits_remaining_as_of).days
+        days_left_on_credits = (credits_expire_date -
+                                credits_remaining_as_of).days
         allowed_credits_per_day = credits_remaining / days_left_on_credits
 
-        relative_to_budget = (total_costs[-1] / allowed_credits_per_day) * 100.0
+        relative_to_budget = (
+            total_costs[-1] / allowed_credits_per_day) * 100.0
 
         if relative_to_budget < 60:
             emoji = ":white_check_mark:"
@@ -123,17 +143,22 @@ def report_cost(event, context):
     else:
         summary = "Yesterday's cost was $%5.2f." % (total_costs[-1])
 
-    hook_url = os.environ.get('SLACK_WEBHOOK_URL')
-    if hook_url:
-        resp = requests.post(
-            hook_url,
-            json={
-                "text": summary + "\n\n```\n" + buffer + "\n```",
-            }
-        )
+    chat_id = os.environ.get('TELEGRAM_USER')
+    telegram_token = os.environ.get('TELEGRAM_BOT_TOKEN')
 
-        if resp.status_code != 200:
-            print("HTTP %s: %s" % (resp.status_code, resp.text))
+    api_url = f"https://api.telegram.org/bot{telegram_token}/"
+
+    params = {'chat_id': chat_id, 'parse_mode': 'Markdown', 'text': summary +
+              "\n\n```\n" + buffer + "\n```"}
+    res = requests.post(api_url + "sendMessage", data=params).json()
+
+    if res["ok"]:
+        return {
+            'statusCode': 200,
+            'body': res['result'],
+        }
     else:
-        print(summary)
-        print(buffer)
+        return {
+            'statusCode': 400,
+            'body': res
+        }
